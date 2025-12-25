5f0cbdead3358355d241dedd006ef438b59425b9
Yakim Gaïes <yakim@MacBook-Pro-de-Yakim.local>
2025-12-25 21:24:57 +0100

    Initial ORANOS CORE server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ORANOS CORE is running ✅");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
